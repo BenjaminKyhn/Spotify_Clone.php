b08c6ef741157b79d879a9294b864d25045dc1ef
Benjamin Kyhn <benkyhn@hotmail.com>
2020-10-04 11:20:50 +0200

    Part 39 complete

--- index.php
+++ index.php
@@ -3,18 +3,21 @@ include("includes/config.php"); //to use sessions
 
 session_destroy(); //force the user to log out
 
-if(isset($_SESSION['userLoggedIn'])){
+if (isset($_SESSION['userLoggedIn'])) {
     $userLoggedIn = $_SESSION['userLoggedIn'];
-}
-else {
+} else {
     header("Location: register.php");
 }
 ?>
 
 <html>
 <head>
-    <title>Spotify Clone</title>
+    <title>Welcome to Slotify</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+<div id="nowPlayingBarContainer">
+
+</div>
 </body>
 </html>
