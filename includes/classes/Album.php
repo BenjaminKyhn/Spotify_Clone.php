a5c58eac27b5ca25426b32e3f3ce240e5e7c86aa
Benjamin Kyhn <benkyhn@hotmail.com>
2020-10-08 19:54:43 +0200

    Part 77 complete

--- includes/classes/Album.php
+++ includes/classes/Album.php
@@ -41,4 +41,15 @@ class Album {
         $query = mysqli_query($this->conn, "SELECT id FROM songs WHERE album='$this->id'");
         return mysqli_num_rows($query);
     }
+
+    public function getSongIds(){
+        $query  = mysqli_query($this->conn, "SELECT id FROM songs WHERE album='$this->id' ORDER BY albumOrder ASC");
+
+        $songArray = array();
+        while ($row = mysqli_fetch_array($query)){
+            array_push($songArray, $row['id']);
+        }
+
+        return $songArray;
+    }
 }
