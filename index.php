af5ca0c38c6695f27595504bb71afa0e6c554392
BenjaminKyhn <benj2605@edu.easj.dk>
2020-10-13 09:55:21 +0200

    Part 119 complete

--- index.php
+++ index.php
@@ -2,27 +2,4 @@
 include("includes/includedFiles.php");
 ?>
 
-
-    <h1 class="pageHeadingBig">You might also like</h1>
-
-    <div class="griViewContainer">
-
-        <?php
-        $albumQuery = mysqli_query($conn, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-
-        while ($row = mysqli_fetch_array($albumQuery)) {
-            echo "<div class='gridViewItem'>
-
-                    <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
-                    <img src='" . $row['artworkPath'] . "'>
-                    
-                    <div class='gridViewInfo'>"
-                        . $row['title'] .
-                    "</div>
-                    </span>
-
-                </div>";
-        }
-        ?>
-
-    </div>
+<script>openPage("browse.php")</script>
