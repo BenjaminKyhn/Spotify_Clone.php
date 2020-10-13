47c1a2762a8ac3b9889b192c8471574de456e237
BenjaminKyhn <benj2605@edu.easj.dk>
2020-10-13 14:09:29 +0200

    Part 141 complete

--- yourMusic.php
+++ yourMusic.php
@@ -21,6 +21,9 @@ include("includes/includedFiles.php");
         }
 
         while ($row = mysqli_fetch_array($playlistsQuery)) {
+
+            $playlist = new Playlist($conn, $row);
+
             echo "<div class='gridViewItem'>
 
                     <div class='playlistImage'>
@@ -28,7 +31,7 @@ include("includes/includedFiles.php");
                     </div>
 
                     <div class='gridViewInfo'>"
-                . $row['name'] .
+                . $playlist->getName() .
                 "</div>
 
                 </div>";
