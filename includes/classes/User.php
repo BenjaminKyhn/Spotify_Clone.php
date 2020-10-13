2034cb3c0884d0a6e687dc10211c6c1803b5197b
BenjaminKyhn <benj2605@edu.easj.dk>
2020-10-13 13:57:49 +0200

    Part 139 complete

---
+++ includes/classes/User.php
@@ -0,0 +1,11 @@
+<?php
+
+class User {
+    private $conn;
+    private $username;
+
+    public function __construct($conn, $username){
+        $this->conn = $conn;
+        $this->username = $username;
+    }
+}
--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -2,9 +2,18 @@
 // This file is used for checking whether the request was sent by ajax or the user manually navigated to that page
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])){ // If request was sent with AJAX
     include("includes/config.php"); //to use sessions
+    include("includes/classes/User.php");
     include("includes/classes/Artist.php");
     include("includes/classes/Album.php");
     include("includes/classes/Song.php");
+
+    if (isset($_GET['userLoggedIn'])){
+        $userLoggedIn = new User($conn, $_GET['userLoggedIn']);
+    }
+    else {
+        echo "Error: Username variable was not passed into the page. Check the openPage() function.";
+        exit();
+    }
 }
 else {
     include("includes/header.php");
