f43da4286daa3f0cb411e26ff8e18ffd6ac4b99b
BenjaminKyhn <benj2605@edu.easj.dk>
2020-10-13 11:02:30 +0200

    Part 128 complete

--- search.php
+++ search.php
@@ -12,6 +12,25 @@ else {
 <div class="searchContainer">
 
     <h4>Search for an artist, album or song</h4>
-    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
+    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="this.value = this.value">
 
 </div>
+
+<script>
+
+    $(".searchInput").focus();
+
+    $(function() {
+        var timer;
+
+        $(".searchInput").keyup(function (){
+            clearTimeout(timer); //Resets the timer every time you type something
+
+            timer = setTimeout(function(){
+                var value = $(".searchInput").val();
+                openPage("search.php?term=" + value);  //TODO: userLoggedIn = undefined is appended to term (it should not be)
+            }, 2000);
+        });
+    });
+
+</script>
