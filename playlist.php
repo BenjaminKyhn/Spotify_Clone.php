b9c675587e05f92546c0a668f2ee45c935eee942
Benjamin Kyhn <benkyhn@hotmail.com>
2020-10-14 21:23:04 +0200

    Part 157 complete

--- playlist.php
+++ playlist.php
@@ -49,7 +49,8 @@ $owner = new User($conn, $playlist->getOwner());
                         </div>
                         
                         <div class='trackOptions'>
-                            <img class='optionsButton' src='assets/images/icons/more.png'>
+                            <input type='hidden' class='songId' value='". $playlistSong->getId() ."'>
+                            <img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
                         </div>
                         
                         <div class='trackDuration'>
@@ -68,3 +69,8 @@ $owner = new User($conn, $playlist->getOwner());
 
     </ul>
 </div>
+
+<nav class="optionsMenu">
+    <input type="hidden" class="songId">
+    <?php echo Playlist::getPlaylistDropdown($conn, $userLoggedIn->getUsername()); ?>
+</nav>
