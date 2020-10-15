8dbc7fd35bf2a66293d7976fe56fd71f5fdf7e66
Benjamin Kyhn <benkyhn@hotmail.com>
2020-10-15 16:07:42 +0200

    Part 160 complete

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -12,4 +12,10 @@ class User {
     public function getUsername(){
         return $this->username;
     }
+
+    public function getFirstAndLastName(){
+        $query = mysqli_query($this->conn, "SELECT concat(firstName, ' ', lastName) as 'name' FROM users WHERE username='$this->username'");
+        $row = mysqli_fetch_array($query);
+        return $row['name'];
+    }
 }
